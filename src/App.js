d7955d9d9faf35fb78818aa3b050b63a054107d9
mansuroff123 <mansuroff145@gmail.com>
2024-02-29 19:50:40 +0500

    hide button and show button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { useState } from 'react'
 function App() {
   const [name, setName] = useState('John')
 
+  const [showContent, setShowContent] = useState(true)
+
   const [events, setEvents] = useState([
     {title: "Hello my name is John", id: 1},
     {title: "John birthday 2000", id: 2},
@@ -13,14 +15,27 @@ function App() {
   const handleClick = () => {
     setName('Doe')
   }
+
+  const handleDelete = (id) => {
+    const filteredEvents = events.filter((event) => {
+      return event.id !== id
+    })
+    setEvents(filteredEvents)
+  }
   return (
     <div className="App">
       <h1>My name is {name}</h1>
       <button onClick={handleClick}>Change name</button>
-      {events.map((event) => {
+      <hr />
+      <br />
+
+      {showContent && <button onClick={() => setShowContent(false)}>Toggle</button>}
+      {!showContent && <button onClick={() => setShowContent(true)}>Toggle</button>}
+      {showContent && events.map((event) => {
         return (
           <div key={event.id}>
             <h2>{event.title}</h2>
+            <button onClick={() => handleDelete(event.id)}>Delete</button>
           </div>
         )
       })}
